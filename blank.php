be31488a42c2852285bd5e4c34341c2c16596553
GabrielNarvadez <gab.narvadez@gmail.com>
2023-08-23 19:01:49 +0800

    Initial Gab Upload

---
+++ blank.php
@@ -0,0 +1,21 @@
+
+<?php require 'assets/global/header.php';?>
+
+<body>
+     <div class="default-padding">
+        <div class="container">
+            <div class="error-box">
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2">
+                        
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php require 'assets/global/footer.php';?>
+
